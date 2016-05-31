92c6ca47102a83fbaf647e7686ebef4c7e1b676d
zxdeng <zengxudeng@gmail.com>
2016-05-31 11:09:35 +0800

    测试

--- app/src/main/java/com/poison/zhihu/MainActivity.java
+++ app/src/main/java/com/poison/zhihu/MainActivity.java
@@ -24,7 +24,6 @@ public class MainActivity extends AppCompatActivity {
         mNavigationMenu.add("测试数据3");
         mNavigationMenu.add("测试数据4");
         mNavigationMenu.add("test");
-        mNavigationMenu.add("test");
     }
 
     @Override
